1b76c5b9b5fa17a9c8f10dca2fcefd5c44564e81
Sergio Rodriguez <se.rodriguez68@gmail.com>
2015-02-23 15:24:09 -0500

    Add validations for descriptions

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,4 +4,8 @@ class TodoList < ActiveRecord::Base
     validates :title, presence: true
     validates :title, length: { minimum: 3 }
 
+    # configura description como un campo requerido y con una longitud mínima de 3 char
+    validates :description, presence: true
+    validates :description, length: { minimum: 5 }
+
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -59,4 +59,44 @@ describe "Creating todo lists" do
         visit "/todo_lists"
         expect(page).to_not have_content("This is what I'm doing today.")
     end
+
+     it "displays an error when the todo list has no description" do
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New todo_list")
+
+        fill_in "Title", with: "Something"
+        fill_in "Description", with: ""
+        click_button "Create Todo list"
+
+
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        expect(page).to_not have_content("Something")
+    end
+
+    it "displays an error when the todo list has a description less than 5 char" do
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New todo_list")
+
+        fill_in "Title", with: "Something"
+        fill_in "Description", with: "foo"
+        click_button "Create Todo list"
+
+
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+
+        visit "/todo_lists"
+        expect(page).to_not have_content("Something")
+    end
+
+
 end

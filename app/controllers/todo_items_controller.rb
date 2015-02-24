51f93cd3a586d779baa61c286bdbcb0e27e03e70
Sergio Rodriguez <se.rodriguez68@gmail.com>
2015-02-24 12:49:16 -0500

    Added test for creating todo items and associated code

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,7 +1,34 @@
 class TodoItemsController < ApplicationController
+#Todas las variables de instancia del controlador están disponibles en el view respectivo
+
   def index
     # permite encontrar el todo list al que el item pertenece a través del route
         # @todo_lilst estará disponible en el view
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def new
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new
+  end
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new(todo_item_params) #relacionado con strong parameters
+    if @todo_item.save
+        flash[:success] = "Added todo list item."
+        redirect_to todo_list_todo_items_path
+    else
+        flash[:error] = "There was a problem adding that todo list item"
+        render action: :new
+    end
+
+  end
+
+  # Le dice a Rails que está bien trabajar con el parámetro content
+    # Strong parameters (documentation)
+  private
+  def todo_item_params
+    params[:todo_item].permit(:content)
   end
 end

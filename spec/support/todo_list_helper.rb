f3f43e6d47cf2b3af810fe51c8d087684bd82d2a
Sergio Rodriguez <se.rodriguez68@gmail.com>
2015-02-25 12:08:07 -0500

    Extract todo list helper 3

---
+++ spec/support/todo_list_helper.rb
@@ -0,0 +1,9 @@
+# CamelCase version of the file name
+module TodoListHelpers
+     def visit_todo_list(list)
+        visit "/todo_lists"
+        within "#todo_list_#{list.id}" do
+            click_link "List Items"
+         end
+    end
+end
